7008aace91770d96e5692a22fb817ee2b2d6ae8d
pwnz54 <kas154723@gmail.com>
2016-03-14 17:53:42 +0600

    added new methods

--- M-s.cpp
+++ M-s.cpp
@@ -4,26 +4,28 @@ class AbstractMatrix
 {
 protected:
 	int n;
-    int m;
-    float* data;
+	int m;
+	float* data;
 public:
-    
-  //AbstractMatrix();
-  ////AbstractMatrix(int m, int n);
-  //  virtual AbstractMatrix& operator+( AbstractMatrix&)=0;
-  //  virtual AbstractMatrix& operator*( AbstractMatrix&)=0;
-  //  virtual AbstractMatrix& operator*( int&)=0;
-  //  virtual AbstractMatrix& operator-( AbstractMatrix&)=0;
-  //  virtual AbstractMatrix& reverse()=0;
-  //  virtual AbstractMatrix& transpose()=0;
-  //  virtual float determinant()=0;
-  //  virtual ostream& print(ostream& o)=0;
-  //  virtual istream& read(istream& o)=0;
-    virtual void set(int i, int j, float data)=0;
-    virtual float get(int i, int j)=0;
-    virtual int getN()=0;
-    virtual int getM()=0;
- //   virtual bool failed()=0;
+
+	//AbstractMatrix();
+	////AbstractMatrix(int m, int n);
+	virtual AbstractMatrix& operator+( AbstractMatrix&)=0;
+	//  virtual AbstractMatrix& operator*( AbstractMatrix&)=0;
+	virtual AbstractMatrix& operator*( int&)=0;
+	virtual AbstractMatrix& operator-( AbstractMatrix&)=0;
+	//  virtual AbstractMatrix& reverse()=0;
+	virtual AbstractMatrix& transpose()=0;
+	//  virtual float determinant()=0;
+	//  virtual ostream& print(ostream& o)=0;
+	//  virtual istream& read(istream& o)=0;
+	virtual void set(int i, int j, float data)=0;
+	virtual float get(int i, int j)=0;
+	virtual int getN()=0;
+	virtual int getM()=0;
+	virtual float get_(int i)=0;
+
+	//   virtual bool failed()=0;
 };
 class matrix : public AbstractMatrix
 {
@@ -43,33 +45,93 @@ public: matrix()
 				data[i] = 1.0*i;
 			}
 		}
-	 float get (int i, int j)
+		float get (int i, int j)
 		{
 			return data[i*n + j];
 		}
-	 void set (int i, int j, float _data)
-	 {
-		 data[i*n + j] = _data;
-	 }
-	 /*virtual matrix& operator* (float& z)
-	 {
-		 matrix* mult = new matrix (n,m);
-		 for (int i(0); i < n*m; i++)
+		void set (int i, int j, float _data)
+		{
+			data[i*n + j] = _data;
+		}
+		virtual AbstractMatrix& operator* (int& z)
+		{
+			matrix* mult = new matrix (n,m);
+			for (int i(0); i < n*m; i++)
 		 {
 			 mult->data[i] = z * data[i];
 		 }
-		
-		 return *mult;
-	 }*/
-	int getN()
-	{ return n;}
-	int getM()
-	{ return m;}
+
+			return *mult;
+		}
+		float get_(int i)
+		{
+			return data[i];
+		}
+		virtual AbstractMatrix& operator=( AbstractMatrix& t)
+		{
+			n = t.getN();
+			m = t.getM();
+			delete data;
+			data = new float [n*m];
+			for(int i = 0; i < n*m; i++)
+		 {
+			 data[i] = t.get_(i);
+		 }
+
+			AbstractMatrix& l = *this;
+			return l;
+		}
+		AbstractMatrix& operator+ (AbstractMatrix& f)
+		{
+			matrix* t = new matrix (f.getN(),f.getM());
+			if (n==f.getN() && m==f.getM())
+			{
+				for (int i(0); i <f.getN()*f.getM(); i++ )
+				{
+					t->data[i] = this->data[i] + f.get_(i);
+				}
+			}
+			return *t;
+		}
+		AbstractMatrix& operator- (AbstractMatrix& f)
+		{
+			matrix* t = new matrix (f.getN(),f.getM());
+			if (n==f.getN() && m==f.getM())
+			{
+				for (int i(0); i <f.getN()*f.getM(); i++ )
+				{
+					t->data[i] = this->data[i] - f.get_(i);
+				}
+			}
+			return *t;
+		}
+
+
+		int getN()
+		{ return n;}
+		int getM()
+		{ return m;}
+		AbstractMatrix& transpose()
+		{
+			matrix* p = new matrix(n,m);
+			for (int i(0); i < n*m; i++)
+			{
+				p->data[i/m + m*(i%m)] = data[i];
+			}
+			return *p;
+
+		}
 };
 int main()
 {
-	matrix S(2,23);
-	float z = 10;
-	cout<<S.getM();
-	
+	matrix S(2,2);
+	int z = 1;
+	matrix mult(2,2);
+	mult = S*z ;
+	matrix Sum (2,2);
+	Sum = S + mult;
+	cout << Sum.get(1,0)<<endl;
+	Sum.transpose();
+	cout << Sum.get(0,0);
+
 }
